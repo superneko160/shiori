07d5fa503d5bee916b82bb7641d310f01f26c4a3
sneko <supernekocat31@gmail.com>
2025-02-11 11:01:38 +0900

    style:fix layout(500 error page)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@
 export default function ErrorPage() {
   return (
     <div className="container mx-auto py-[50px]">
-      <h2 className="text-center text-[50px]">500 - Internal Server Error</h2>
+      <h2 className="text-center text-2xl">500 - Internal Server Error</h2>
     </div>
   )
 }
