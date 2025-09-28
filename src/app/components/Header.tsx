35f692b0364339f61954fc843823c2c7e32fddac
sneko <supernekocat31@gmail.com>
2025-09-29 01:17:03 +0900

    style: enhance sign-in button with Google icon and update user name display in menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -17,8 +17,14 @@ function SignIn() {
         await signIn("google")
       }}
     >
-      <button className="h-10 w-24 rounded-full bg-indigo-500 px-4 py-2 font-medium text-slate-100 hover:bg-indigo-400">
-        Sign in
+      <button className="flex h-10 w-52 items-center justify-center gap-2 rounded-full border border-indigo-500 px-1 py-2 font-medium text-indigo-500 hover:bg-indigo-500 hover:text-slate-100">
+        Sign in with Google
+        <Image
+          width={24}
+          height={24}
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google icon"
+        />
       </button>
     </form>
   )
@@ -42,6 +48,9 @@ function UserMenu({ user }: { user: Session["user"] }) {
         )}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+        {user.name && (
+          <div className="border-b border-slate-200 p-1">{user.name}</div>
+        )}
         <DropdownMenuItem>
           <form
             action={async () => {
